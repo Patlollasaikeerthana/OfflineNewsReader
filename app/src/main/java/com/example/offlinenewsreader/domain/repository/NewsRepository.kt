002c58fdd047ea950599f872d42a220a2aa6d688
Patlollasaikeerthana <84503313+Patlollasaikeerthana@users.noreply.github.com>
2026-05-02 21:28:57 +0530

    Update NewsRepository.kt

--- app/src/main/java/com/example/offlinenewsreader/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/offlinenewsreader/domain/repository/NewsRepository.kt
@@ -4,6 +4,7 @@ import com.example.offlinenewsreader.domain.model.Article
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
+    fun getNewsPaged():Flow<PagingData<Article>>
     fun getNews(): Flow<List<Article>>
     fun searchNews(query: String): Flow<List<Article>>
     suspend fun refresh()
