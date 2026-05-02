7dfa5272786bfd1f5cf916a1037fc182b56bca81
Patlollasaikeerthana <84503313+Patlollasaikeerthana@users.noreply.github.com>
2026-05-02 22:10:20 +0530

    Update NewsRepositoryImpl.kt

--- app/src/main/java/com/example/offlinenewsreader/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/offlinenewsreader/data/repository/NewsRepositoryImpl.kt
@@ -1,7 +1,12 @@
 package com.example.offlinenewsreader.data.repository
 
 import androidx.room.util.query
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.map
 import com.example.offlinenewsreader.data.local.NewsDao
+import com.example.offlinenewsreader.data.local.NewsDataBase
+import com.example.offlinenewsreader.data.local.RemoteKeyDao
 import com.example.offlinenewsreader.data.mapper.toDomain
 import com.example.offlinenewsreader.data.mapper.toEntity
 import com.example.offlinenewsreader.data.remote.NewsApi
@@ -14,8 +19,25 @@ import kotlin.collections.map
 
 class NewsRepositoryImpl(
     private val api: NewsApi,
-    private val dao: NewsDao
+    private val dao: NewsDao,private val remoteKeyDao:RemoteKeyDao,
+    private val db:NewsDataBase
 ) : NewsRepository {
+    
+    @OptIn(ExperimentalPagingApi::class)
+    override fun getNewsPaged(): Flow<PagingData<Article>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 10,
+                prefetchDistance = 2,       // load next page when 2 items from end
+                enablePlaceholders = false
+            ),
+            remoteMediator = NewsRemoteMediator(api, dao, remoteKeyDao, db),
+            pagingSourceFactory = { dao.getNewsPaged() }
+        ).flow.map { pagingData ->
+            pagingData.map { it.toDomain() }
+        }
+    }
+    
     override fun getNews(): Flow<List<Article>> {
         return networkBoundResource(
             query = { dao.getNews().map { it.map { e -> e.toDomain() } } },
