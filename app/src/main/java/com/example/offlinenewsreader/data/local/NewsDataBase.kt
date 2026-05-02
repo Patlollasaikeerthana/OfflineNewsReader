03e7e82063ab18e7ed06268b955ee9e5b5b985d4
Patlollasaikeerthana <84503313+Patlollasaikeerthana@users.noreply.github.com>
2026-05-02 22:17:56 +0530

    Update NewsDataBase.kt

--- app/src/main/java/com/example/offlinenewsreader/data/local/NewsDataBase.kt
+++ app/src/main/java/com/example/offlinenewsreader/data/local/NewsDataBase.kt
@@ -3,7 +3,8 @@ package com.example.offlinenewsreader.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ArticleEntity::class], version = 1)
+@Database(entities = [ArticleEntity::class,RemoteKeyEntity::class], version = 2)
 abstract class NewsDataBase : RoomDatabase() {
     abstract fun newsDao(): NewsDao
+    abstract fun remoteKeyDao():RemoteKeyDao
 }
