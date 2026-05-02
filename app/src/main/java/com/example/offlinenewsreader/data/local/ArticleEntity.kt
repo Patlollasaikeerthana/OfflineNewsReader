9625b420ad7b243db185104d9dbc789df1d7d712
Patlollasaikeerthana <84503313+Patlollasaikeerthana@users.noreply.github.com>
2026-05-02 22:13:22 +0530

    Update ArticleEntity.kt

--- app/src/main/java/com/example/offlinenewsreader/data/local/ArticleEntity.kt
+++ app/src/main/java/com/example/offlinenewsreader/data/local/ArticleEntity.kt
@@ -9,5 +9,13 @@ data class ArticleEntity(
     val title: String,
     val description: String?,
     val imageUrl: String,
-    val isBookMarked: Boolean = false
+    val isBookMarked: Boolean = false,
+    val page:Int=1
+)
+
+@Entity(tableName="remote_keys")
+data class RemoteKeyEntity(
+    @PrimaryKey val url:String,
+    val prevPage:Int?,
+    val nextPage:Int?
 )
